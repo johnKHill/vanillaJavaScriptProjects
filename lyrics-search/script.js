09ee28c6c59b484eab21fae8c42e2a9cc934ac8d
Johknotter Hill <johknotterhill@Johknotters-MacBook-Pro.local>
2020-05-30 16:20:11 -0400

    Ui is complete. Cached DOM objects, added form event listener. Using Lyrics API for fetching a song or artist with Async Await. Will be creating a method to display the song to the DOM

---
+++ lyrics-search/script.js
@@ -0,0 +1,35 @@
+// Caching objects from DOM
+// -------------------------------------
+const form = document.getElementById("form");
+const search = document.getElementById("search");
+const result = document.getElementById("result");
+const more = document.getElementById("more");
+
+const apiURL = "https://api.lyrics.ovh";
+
+//                               Methods
+// -------------------------------------
+// Search by song or artist
+async function searchSongs(term) {
+  const res = await fetch(`${apiURL}/suggest/${term}`);
+  const data = await res.json();
+
+  showData();
+}
+
+// Show song and artist in DOM
+function showData(data) {}
+
+// Event Listeners
+// -------------------------------------
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const searchTerm = search.value.trim();
+
+  if (!searchTerm) {
+    alert("Please type in a search term");
+  } else {
+    searchSongs(searchTerm);
+  }
+});
